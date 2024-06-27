9b8898f666b3eb0c3b69c60bfc13c6370029d648
Janina HC <janinahc@outlook.com>
2024-06-27 16:06:41 +0200

    cleaned a bit

--- scripts/generate_ssh_keys.sh
+++ scripts/generate_ssh_keys.sh
@@ -6,7 +6,7 @@
 ssh-keygen -t ed25519 -C "ansible" -f ~/.ssh/ansible -N ""
 
 # Generate personal SSH key
-ssh-keygen -t ed25519 -C "Strong Default" -f ~/.ssh/id_ed25519
+ssh-keygen -t ed25519 -C "Default" -f ~/.ssh/id_ed25519
 
 echo "SSH keys have been generated."
 echo "Remember to copy the public keys to your target servers."
