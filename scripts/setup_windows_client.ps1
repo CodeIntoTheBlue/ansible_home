ce14b84fdb895320a172b5234b29827fd06edf24
Janina HC <janinahc@outlook.com>
2024-06-28 15:53:35 +0200

    ..

--- scripts/setup_windows_client.ps1
+++ scripts/setup_windows_client.ps1
@@ -1,7 +1,7 @@
 # setup_windows_client.ps1
 # 
 
-# Downloading the configurationscript
+# Downloading the configurationscript for winrm and installing it
 $url = “https://raw.githubusercontent.com/ansible/ansible-documentation/devel/examples/scripts/ConfigureRemotingForAnsible.ps1” 7
 $file = “$env:temp\ConfigureRemotingForAnsible.ps1”
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
